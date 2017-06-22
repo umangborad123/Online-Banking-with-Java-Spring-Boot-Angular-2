e220a7c6e1bd86b9d0af37cd72201d2459384631
Umang <umangborad123@gmail.com>
2017-06-22 14:42:45 -0400

    Modifying RequestFilter

--- UserFront/src/main/java/com/userFront/config/RequestFilter.java
+++ UserFront/src/main/java/com/userFront/config/RequestFilter.java
@@ -20,7 +20,7 @@ public class RequestFilter implements Filter {
         HttpServletResponse response = (HttpServletResponse) res;
         HttpServletRequest request = (HttpServletRequest) req;
 
-        response.setHeader("Access-Control-Allow-Origin", "http://localhost:4200");
+        response.setHeader("Access-Control-Allow-Origin", "http://localhost:4200");  //8081
         response.setHeader("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS, DELETE");
         response.setHeader("Access-Control-Allow-Headers", "x-requested-with");
         response.setHeader("Access-Control-Max-Age", "3600");
